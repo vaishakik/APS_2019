e5fa6bd6bec03cd9f88c95b56d9331c4c3183b30
Why?shock! <vaishak.i.kuppast@gmail.com>
2019-03-16 20:21:40 +0530

    Create Beautiful_triplets(hacker_rank).py

---
+++ CODES/Beautiful_triplets(hacker_rank).py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulTriplets function below.
+def beautifulTriplets(d, arr):
+    c=0
+    for i in range(n):
+        if arr[i]+d in arr and arr[i]+2*d in arr:
+            c+=1
+    return  (c)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
